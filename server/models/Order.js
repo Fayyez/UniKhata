5c623915bf11b23bd7a7078df915fa306ba6f29a
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-04 18:38:57 +0500

    Revert "Merge branch 'backend' into frontend"
    This reverts commit fc3b3777464ff69ced6723837aad0e63ad6038dc, reversing
    changes made to 75879d2788fe07abccbd8dfec8454e63379bceb0.

--- server/models/Order.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-// TODO: schema yet to be implemented for order
-const orderSchema = new mongoose.Schema({
-    store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', required: true},
-});
--- server/models/ProductCounter.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const counterSchema = new mongoose.Schema({
-    store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', required: true, unique: true },
-    lastProductId: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('ProductCounter', counterSchema);
