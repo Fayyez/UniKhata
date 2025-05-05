f4dd0b2cbb7a4adc464bd05916f39bbeb63dd46f
fayyez <fayyez2056@gmail.com>
2025-05-05 22:41:58 +0500

    implemented factories and added skeleton dummy store and courier class

--- server/utils/courierFactory.js
+++ server/utils/courierFactory.js
@@ -7,9 +7,9 @@ import DummyCourier from "../integration/Couriers/DummyCourier.js";
  *
  * @param {Array} list_of_couriers_from_database - Array of CourierIntegration objects from the database.
  * Each object should have a `title` field that matches a courier title defined in constants.js.
- * 
+ *
  * @returns {Array} - An array of instantiated courier class objects corresponding to the titles.
- * 
+ *
  * Example:
  * Input: [
  *   { title: "DUMMY_COURIER", ...otherFields },
@@ -22,17 +22,30 @@ import DummyCourier from "../integration/Couriers/DummyCourier.js";
  */
 function createCourierObjects(list_of_couriers_from_database) {
     const courierObjects = [];
-
-    list_of_couriers_from_database.forEach(courierIntegrationItem => {
-        switch (courierIntegrationItem.title) {
-            case DUMMY_COURIER:
-                courierObjects.push(new DummyCourier(courierIntegrationItem));
-                break;
-            // Add more cases here for other courier titles and their corresponding classes
-            default:
-                console.warn(`No matching courier class found for title: ${courierIntegrationItem.title}`);
-        }
-    });
+    try {
+        list_of_couriers_from_database.forEach((courierIntegrationItem) => {
+            switch (courierIntegrationItem.title) {
+                case DUMMY_COURIER:
+                    courierObjects.push(new DummyCourier(courierIntegrationItem));
+                    break;
+                // Add more cases here for other courier titles and their corresponding classes
+                default:
+                    console.warn(
+                        `No matching courier class found for title: ${courierIntegrationItem.title}`
+                    );
+                    // throw error that this courier service is not supported
+                    throw new Error(
+                        `Courier service ${courierIntegrationItem.title} is not supported.`
+                    );
+            }
+        });
+    } catch (error) {
+        console.error("Error creating courier objects:", error);
+        // Handle the error as needed, e.g., log it, rethrow it, etc.
+        throw new Error(
+            "Failed to create courier objects. Please check the input data."
+        );
+    }
 
     return courierObjects;
 }
--- server/utils/estoreFactory.js
+++ server/utils/estoreFactory.js
@@ -1,15 +1,61 @@
-import ecommerceIntegration from "../models/EcommerceIntegration.js";
-// import all the class from following folder
-//import * from "../integration/E-stores/"
-// import all titles from the "./constants.js" file
-import {DUMMY_STORE, _} from "./constants.js";
+import { DUMMY_STORE } from "./constants.js";
+// Import all the e-store classes
+import DummyStore from "../integration/E-stores/DummyStore.js";
 
-// TODO: implement ecommerce store factory
-function createEStoreObjects(list_of_estores_from_database) {
-    // for each ecommerceIntegration item passed to this function
-    // check title and create the corresponding class object (titles are defined in the constants file and classes are in the integration/E-stores folder)
+/**
+ * Creates a list of e-commerce store class objects based on the provided list of EcommerceIntegration objects.
+ *
+ * @param {Array} list_of_ecommerceIntegration_objects - Array of EcommerceIntegration objects from the database.
+ * Each object should have a `title` field that matches a store title defined in constants.js.
+ *
+ * @returns {Array} - An array of instantiated e-store class objects corresponding to the titles.
+ *
+ * Example:
+ * Input: [
+ *   { title: "DUMMY_STORE", ...otherFields },
+ *   { title: "ANOTHER_STORE", ...otherFields }
+ * ]
+ * Output: [
+ *   DummyStore instance,
+ *   AnotherStore instance
+ * ]
+ */
+function createEStoreObjects(list_of_ecommerceIntegration_objects) {
+    const eStoreObjects = [];
+    try {
+        list_of_ecommerceIntegration_objects.forEach((ecommerceIntegrationItem, ind) => {
+            switch (ecommerceIntegrationItem.title) {
+                case DUMMY_STORE:
+                    eStoreObjects.push(new DummyStore(ecommerceIntegrationItem));
+                    break;
+                // Add more cases here for other e-store titles and their corresponding classes
+                default:
+                    console.warn(
+                        `No matching e-store class found for title: ${ecommerceIntegrationItem.title}`
+                    );
+                    // throw error that this e-store service is not supported
+                    throw new Error(
+                        `E-store service ${ecommerceIntegrationItem.title} is not supported.`
+                    );
+            }
+        });
+    } catch (error) {
+        console.error("Error creating e-store objects:", error);
+        // Handle the error as needed, e.g., log it, rethrow it, etc.
+        throw new Error(
+            "Failed to create e-store objects. Please check the input data."
+        );
+    }
 
-    // return list of estore objects
+    return eStoreObjects;
 }
 
 export default createEStoreObjects;
+
+// //// test the function
+// // const testEcommerceIntegrationObjects = [
+// //     { title: DUMMY_STORE, token: "dummy-token", email: "email1@gmail.com", apiEndpoint: "https://dummyapi.com" },
+// //     { title: DUMMY_STORE, token: "another-token", email: "email2@gmail.com", apiEndpoint: "https://           anotherapi.com" },
+// // ];
+// // const eStoreObjects = createEStoreObjects(testEcommerceIntegrationObjects);
+// // console.log("E-store objects created:", eStoreObjects);
