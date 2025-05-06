6a40a9d4e44389be3450ebf55343bf3fe0a9240c
eehab-saadat <eehab.saadat@gmail.com>
2025-05-07 00:00:40 +0500

    updated order schema

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid"; // Import the uuid library
 
-// TODO: schema yet to be implemented for order
+const productEntrySchema = new mongoose.Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, // required to be filled
+    quantity: { type: Number, required: true }, // required to be filled
+}, { _id: false });
+
 const orderSchema = new mongoose.Schema({
-    store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', required: true},
+    store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', required: true },
+    orderId: { type: String, required: true, unique: true, default: uuidv4 }, // Generate a unique id by default
+    productEntries: [productEntrySchema], // required to be filled
+    platform:  { type: mongoose.Schema.Types.ObjectId, ref: 'ECommerceIntegration', required: true }, // required to be filled
+    courier: { type: mongoose.Schema.Types.ObjectId, ref: 'CourierIntegration' }, // required to be filled
+    status: { type: String, required: true }, // required to be filled
 });
