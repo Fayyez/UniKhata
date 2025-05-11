4bfe0d3321cfc89f3f54738885a3f10bdc754544
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-12 03:39:32 +0500

    .

--- web/src/store/slices/orderSlice.ts
+++ web/src/store/slices/orderSlice.ts
@@ -45,7 +45,7 @@ export const fetchOrders = createAsyncThunk<Order[], FetchOrdersParams, { reject
   'order/fetchOrders',
   async ({ uid, sid }, { rejectWithValue }) => {
     try {
-      await axiosInstance.get(`/orders/new?sid=${sid}`);
+      await axiosInstance.get(`/orders/new/?sid=${sid}`);
       const response = await axiosInstance.request({
         url: '/orders/',
         method: 'get',
