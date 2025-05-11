692b5ce7ae6af3e7c5cfeb4e31ce8ffd3785b7cd
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-11 23:55:47 +0500

    splash screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,63 @@
-import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
-import ThemeToggle from "./components/ThemeToggle";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import DashboardPage from "./pages/DashboardPage";
-import ProfilePage from "./pages/ProfilePage";
-import CreateStorePage from "./pages/CreateStorePage";
-import StorePage from "./pages/StorePage";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import StorePage from './pages/StorePage';
+import CreateStorePage from './pages/CreateStorePage';
+import ProfilePage from './pages/ProfilePage';
+import UnauthorizedPage from './pages/UnauthorizedPage';
+import SplashScreen from './pages/SplashScreen';
+import { useSelector } from 'react-redux';
+import type { RootState } from './store';
 
-function App() {
-  return (
-    <ThemeProvider>
-      <div className="min-h-screen bg-white dark:bg-gray-900">
-        <ThemeToggle />
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/create-store" element={<CreateStorePage />} />
-          <Route path="/store/:id" element={<StorePage />} />
-        </Routes>
+const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+  
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="text-xl">Loading...</div>
       </div>
-    </ThemeProvider>
+    );
+  }
+  
+  return user ? <>{children}</> : <Navigate to="/login" />;
+};
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={
+          <PrivateRoute>
+            <DashboardPage />
+          </PrivateRoute>
+        } />
+        <Route path="/store/:id" element={
+          <PrivateRoute>
+            <StorePage />
+          </PrivateRoute>
+        } />
+        <Route path="/create-store" element={
+          <PrivateRoute>
+            <CreateStorePage />
+          </PrivateRoute>
+        } />
+        <Route path="/profile" element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        } />
+        <Route path="/unauthorized" element={<UnauthorizedPage />} />
+      </Routes>
+    </Provider>
   );
-}
+};
 
 export default App;
